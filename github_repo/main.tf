c121bcad65ced4a71474e0190e4c0fdf0a8c7fb1
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2022-09-01 15:56:46 +0200

    chore: update core

--- github_repo/main.tf
+++ github_repo/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.24.0"
+      version = "~> 4.31.0"
     }
   }
   required_version = ">= 1.2"
