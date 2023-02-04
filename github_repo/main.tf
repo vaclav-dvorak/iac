d939e0ff11032c68dec1c563acaa3ab6a0f51d01
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2023-02-04 21:18:00 +0100

    feat: Update providers and tfcore + add 2 repos

--- github_repo/main.tf
+++ github_repo/main.tf
@@ -1,19 +1,11 @@
 terraform {
-  #backend "pg" {
-  #  schema_name = "github_repo_state"
-  #}
+  # backend "local" {}
   backend "azurerm" {
     subscription_id      = "504cc02b-11a6-43b5-a670-117bc2a9c30a"
     resource_group_name  = "devops"
     storage_account_name = "stvddevops"
     container_name       = "tfstate"
     key                  = "github/repos.tfstate"
-  }
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.31.0"
-    }
   }
   required_version = ">= 1.2"
 }
